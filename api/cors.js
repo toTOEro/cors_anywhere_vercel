7ceafc7dd8552d2bdf708223f2bdeb14f3da913a
toTOEro <43686336+toTOEro@users.noreply.github.com>
2022-07-27 13:55:21 -0700

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,5 +1,13 @@
 export default async function handler(request, response) {
-  
+  response.setHeader('Access-Control-Allow-Credentials', true)
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   const https = require('https');
   let query = Object.entries(request.query);
   query.shift();
